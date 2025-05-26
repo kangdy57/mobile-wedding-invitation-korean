a43707473ef8c43619c8f5ecb32e7220893a6e23
kangdy57 <kangdy57@gmail.com>
2025-05-26 13:19:57 +0200

    initial push

--- src/pages/Bride.js
+++ src/pages/Bride.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import data from '../assets/image_data.json';
 import pinIcon from '../assets/location-pin.png';
 import brideAccountData from '../assets/bride_account_number_data.json';
-import groomAccountData from '../assets/groom_account_number_data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container as MapDiv, NaverMap, Marker, useNavermaps} from 'react-naver-maps';
 import '../App.css';
@@ -74,27 +73,36 @@ function Bride() {
                 <img src="https://dave-khim-aws-bucket-public.s3.ap-northeast-2.amazonaws.com/test/love-tenderness-couple-s-crossed-hands.jpg" className='main-image' alt='t1'></img>
               </div>
               <div className='mainsection-text'>
-                <div className='mainsection-text-1'>결혼식에 초대합니다</div>
+                <div className='mainsection-text-1'>저희 결혼합니다</div>
                 <div className='mainsection-text-2'>
-                  김신랑 <span className='text2-inner'> & </span> 이신부
+                  강다연 <span className='text2-inner'> & </span> 프라노이 물미
                 </div>
-                <div className='mainsection-text-3'>2023. 00. 0 토요일 오전 11시<br/>00대학교 웨딩홀</div>
+                <div className='mainsection-text-3'>2026. 10. 24 토요일 15시<br/>세종대왕기념관</div>
               </div>
             </div>
             <div className='invitation-section'>
               <div className='invitation-section-text1'>INVITATION</div>
               <div className='invitation-section-text2'>
-                    저희 두 사람이 사랑과 믿음으로<br/>
+                    한국에서 태어난 다연과<br/>
+                    네팔에서 태어난 프라노이가<br/>
+                    독일에서 운명처럼 만나<br/>
                     한 가정을 이루게 되었습니다.<br/>
-                    바쁘시더라도 부디 오셔서<br/>
-                    저희의 앞날을 축복해 주시고<br/>
-                    격려해 주시면 감사하겠습니다.
+                    저희의 출발을 알리는 예식에<br/>
+                    귀한 걸음 부탁드립니다.
               </div>
               <div className='invitation-section-text3'>
-                김아빠・이엄마<span className='text3-inner'>의 장남</span> 신랑
+                강정배・진효정<span className='text3-inner'>의 딸</span> 강다연
               </div>
               <div className='invitation-section-text3'>
-                이아빠・박엄마<span className='text3-inner'>의 차녀</span> 신부
+                람 물미・저너히타 물미<span className='text3-inner'>의 아들</span> 프라노이 물미 
+              </div>
+    <br/>
+    <br/>
+                <div className='invitation-section-text3'>
+                Jungbae Kang・Hyojung Jin<span className='text3-inner'>'s Daughter</span> Dayeon Kang
+              </div>
+              <div className='invitation-section-text3'>
+                Ram Mulmi・Janahita Mulmi<span className='text3-inner'>'s Son</span> Prannoy Mulmi
               </div>
             </div>
             <div className='gallery-section'>
@@ -130,10 +138,10 @@ function Bride() {
                 }}
               >
                 <NaverMap 
-                  defaultCenter={new navermaps.LatLng(37.44865592343993,126.95097244672262)}
+                  defaultCenter={new navermaps.LatLng(37.59080787175967, 127.04353764771166)}
                   defaultZoom={16}>
                   <Marker 
-                  position={new navermaps.LatLng(37.44865592343993,126.95097244672262)} 
+                  position={new navermaps.LatLng(37.59080787175967, 127.04353764771166)} 
                   icon={
                     {
                       url : pinIcon,
@@ -144,35 +152,30 @@ function Bride() {
               </MapDiv>
             </div>
             <div className='location-info-section'>
-                <div className='location-info-section-text1'>이라운지 서울대점</div>
+                <div className='location-info-section-text1'>세종대왕기념관</div>
                 <div className='location-info-section-text2'>
-                    서울특별시 관악구 관악로 1<br/>
-                    서울대학교 310동 엔지니어하우스<br/>
-                    Tel. 02-875-7761
+                    서울 동대문구 회기로 56<br/>
+                    Tel. 02-960-1700
                 </div>
             </div>
             <div className='location-how-publictrans-section'>
               <div className='location-how-publictrans-section-text1'>대중교통</div>
               <ol className='location-how-publictrans-section-list'>
-                <li>2호선 서울대입구역 3번 출구 → 5511,5513 버스 → 제2공학관(종점) 하차</li>
-                <li>2호선 낙성대역 4번 출구 → 관악02 마을버스 → 제2공학관(종점) 하차</li>
-                <li>신림선 관악산역 1번 출구 → 5511,5516 버스 → 제2공학관(종점) 하차</li>
+                <li>6호선 고려대역 3번 출구 (도보 10분 or 15분 간격 셔틀버스 운영)</li>
+
               </ol>
             </div>
             <div className='location-how2-section'>
               <div className='location-how2-section-text1'>자가용</div>
               <div className='location-how2-section-text2'>
-                네비게이션 이용 시 “이라운지 서울대점”을 입력하세요. (주차 2시간 무료)
+                네비게이션 이용 시 “세종대왕기념관”을 입력하세요. (주차 무료)
               </div>
             </div>
             <div className='congratulatory-section'>
               <div className='congratulatory-section-text'>마음 전하실 곳</div>
                 <div 
-                  className='congratulatory-section-btn' 
-                  onClick={() => accountClick(groomAccountData)}>신랑측 계좌번호</div>
-                <div 
                   className='congratulatory-section-btn'
-                  onClick={() => accountClick(brideAccountData)}>신부측 계좌번호</div>
+                  onClick={() => accountClick(brideAccountData)}>신부 측 계좌번호</div>
             </div>
             {clickedAccountData && <AccountModal 
               clickedAccountData={clickedAccountData}
