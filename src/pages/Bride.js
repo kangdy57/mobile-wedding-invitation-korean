7ce2f41e3c5a1cbca336fbcbd1557f8721e15b0e
kangdy57 <kangdy57@gmail.com>
2025-05-26 21:35:56 +0200

    안내문구 등 수정

--- src/pages/Bride.js
+++ src/pages/Bride.js
@@ -80,12 +80,12 @@ function Bride() {
             <div className='invitation-section'>
               <div className='invitation-section-text1'>INVITATION</div>
               <div className='invitation-section-text2'>
-                    한국에서 태어난 다연과<br/>
-                    네팔에서 태어난 프라노이가<br/>
-                    독일에서 운명처럼 만나<br/>
+                    <strong>한국</strong>에서 태어난 <strong>다연</strong>과<br/>
+                    <strong>네팔</strong>에서 태어난 <strong>프라노이</strong>가<br/>
+                    <strong>독일</strong>에서 운명처럼 만나<br/>
                     한 가정을 이루게 되었습니다.<br/>
-                    저희의 출발을 알리는 예식에<br/>
-                    귀한 걸음 부탁드립니다.
+                    저희의 출발을 알리는 <strong>전통혼례식</strong>에<br/>
+                    함께 해 주시면 감사하겠습니다.
               </div>
               <div className='invitation-section-text3'>
                 강정배・진효정<span className='text3-inner'>의 딸</span> 강다연
@@ -141,6 +141,15 @@ function Bride() {
               <div className='location-how2-section-text2'>
                 네비게이션 이용 시 “세종대왕기념관”을 입력하세요. (주차 무료)
               </div>
+            </div>
+            <div className='general-info-section'>
+                <div className='general-info-section-text1'>안내 드립니다</div>
+                <div className='general-info-section-text2'>
+                    본 예식에는 외국인 하객분들도 다수 참석할 예정입니다.<br/>
+                    한국의 아름다움을 직접 느끼고자 많은 분들께서 한복을 입고 참석하실 예정이오니, <br/>
+                    평소 소장하고 계시던 한복이 있으시다면 부담 없이 착용해 주셔도 좋겠습니다.<br/>
+                    물론 한복이 아니어도 전혀 무방하오니, 편안한 마음으로 참석해 주시면 감사하겠습니다.<br/>
+                </div>
             </div>
             <div className='congratulatory-section'>
               <div className='congratulatory-section-text'>마음 전하실 곳</div>
