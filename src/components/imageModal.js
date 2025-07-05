6aba4bc7f83352a67f81767c9258f540c8b28cf7
kangdy57 <kangdy57@gmail.com>
2025-07-05 18:32:16 +0200

    click, css

--- src/components/imageModal.js
+++ src/components/imageModal.js
@@ -1,32 +1,32 @@
 import { ReactComponent as LeftArrow } from '../assets/arrow-left.svg';
 import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
 
-const imageModal = ({clickedImg, handleRotationRight, handleRotationLeft, setClickedImg}) => {
-
-    const handleClick = (e) => {
-        if (e.target.classList.contains("dismiss")) {
-            setClickedImg(null);
-        }
+const ImageModal = ({ clickedImg, handleRotationRight, handleRotationLeft, setClickedImg }) => {
+  const handleClick = (e) => {
+    if (e.target.classList.contains("dismiss")) {
+      setClickedImg(null);
     }
+  };
 
-    return <>
+  // Only render the modal if clickedImg is set
+  if (!clickedImg) return null;
+
+  return (
     <div className="overlay dismiss" onClick={handleClick}>
-        <img className="" src={clickedImg} alt="original size"/>
-        <span className="dismiss" onClick={handleClick}>X</span>
-        <div onClick={handleRotationRight}>
-            <div>
-                <RightArrow className="overlay-arrows_right"
-                width="50" height="50"/>
-            </div>
-        </div>
-        <div onClick={handleRotationLeft}>
-            <div>
-                <LeftArrow className="overlay-arrows_left"
-                width="50" height="50"/>
-            </div>
-        </div>
+      <img
+        loading="lazy"
+        src={clickedImg}
+        alt="original size"
+      />
+      <span className="dismiss" onClick={handleClick}>X</span>
+      <div onClick={handleRotationRight}>
+        <RightArrow className="overlay-arrows_right" width="50" height="50" />
+      </div>
+      <div onClick={handleRotationLeft}>
+        <LeftArrow className="overlay-arrows_left" width="50" height="50" />
+      </div>
     </div>
-    </>;
+  );
 };
 
-export default imageModal;
+export default ImageModal;
